227355d6c404ad395b73175f813c374c5cb91f43
HarriDonaie <harridonaie@gmail.com>
2026-09-03 22:23:22 +0100

    3.2 - Draw Player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -13,6 +14,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -21,6 +23,7 @@ def main():
                 return
             pass
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
