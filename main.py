6d0ffe722af11e8c7e0325b26237c880f7e91880
HarriDonaie <harridonaie@gmail.com>
2026-09-03 22:10:55 +0100

    2.4 - FPS

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -18,7 +22,8 @@ def main():
             pass
         screen.fill("black")
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
+        
 
 
 
