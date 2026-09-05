832a7551bdc843ad23f776dfb9b41366ae618817
HarriDonaie <harridonaie@gmail.com>
2026-09-05 23:35:04 +0100

    3.5 - Groups

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -23,8 +28,12 @@ def main():
                 return
             pass
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for dbl in drawable:
+            dbl.draw(screen)
+
+        #player.update(dt)
+        #player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
